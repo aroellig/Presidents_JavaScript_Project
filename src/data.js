e2a5ddf71bafd72b43cf6a3669bb82fa2dcb9972
your name here <roelliga@yahoo.com>
2022-01-24 16:55:11 -0800

    presidents data

--- src/data.js
+++ src/data.js
@@ -1,1 +1,56 @@
 const presName = ["George Washington", "John Adams", "Thomas Jefferson", "James Madison", "James Monroe", "John Quincy Adams", "Andrew Jackson", "Martin Van Buren", "William Henry Harrison", "John Tyler", "James Polk", "Zachary Taylor", "Millard Fillmore", "Franklin Pierce", "James Buchanan", "Abraham Lincoln", "Andrew Johnson", "Ulysses Grant", "Rutherford Hayes", "James Garfield", "Chester Arthur", "Grover Celveland", "Benjamin Harrison", "William McKinley", "Theordore Roosevelt", "William Taft", "Woodrow Wilson", "Warren Harding", "Calvin Coolidge", "Herbert Hoover", "Franklin Roosevelt", "Harry Truman", "Dwight Eisenhower", "John Kennedy", "Lyndon Johnson", "Richard Nixon", "Gerald Ford", "Jimmy Carter", "Ronald Reagan", "George H. W. Bush", "Bill Clinton", "George W. Bush", "Barrack Obama", "Donald Trump", "Joe Biden"]
+
+
+presData={
+    "George Washington": "He is the only US president to have personally led US soldiers into battle while he was serving as the President of the United States",
+    "John Adams": "He created the United States Marine Band in 1798 which is the oldest professional musical association in the US",
+    "Thomas Jefferson": "He loved to listen to Mocking Birds sing and he kept them as pets",
+    "James Madison": "While studying at Princeton he stayed for an extra year which makes him the very first postgraduate student in the US",
+    "James Monro": "Monroe was the US president who purchased Florida from the Spanish for five million",
+    "John Quincy Adams": "Every morning he would swim in the Potamac river for exercise",
+    "Andrew Jackson": "He is the only president to have personally thwarted an assination attempt on his life",
+    "Martin Van Buren": "He was the first US president to have been born in the United States when it was its own country and not a British colony",
+    "William Henry Harrison": "He only served as president for one month, after which he died of pneumonia",
+    "John Tyler": "He had fifteen children the most of any president",
+    "James Polk": "For two days every week James Polk held office hours in which any citizen could visit him",
+    "Zachary Taylor": "He was unaware that he was nominated as presential candidate until he recieved a letter telling him so",
+    "Millard Fillmore": "He did not have a vice president while he was president",
+    "Franklin Pierce": "When he was twelve his father made him walk for miles in a thunderstorm",
+    "James Buchanan": "He is the only president who has never been married",
+    "Abraham Lincoln": "Was a very accomplished wrestler and only ever lost one out of three hundred matches",
+    "Andrew Johnson": "Was the first president to ever be impeached",
+    "Ulysses Grant": "He was a gifted writer and his autobiography is considered to be one of the best ever written by a former president",
+    "Rutherford Hayes": "He and his wife started the tradition of having Easter Egg rolls on the Whitehouse lawn",
+    "James Garfield": "Married one of his students while he was teaching classes at a college",
+    "Chester Arthur": "He was a very skilled fisherman",
+    "Grover Cleveland": "Is the only US president to have served two non consectutive terms",
+    "Benjamin Harrison": "Was afraid to touch lightbulbs out of fear of electrocution",
+    "William McKinley": "He was the first president to ever ride in an automobile",
+    "Theodore Roosevelt": "He was blinded in one eye while boxing in the Whitehouse",
+    "William Taft": "Was the fattest president and once got stuck in the Whitehouse bathtub",
+    "Woodrow Wilson": "Was the first president from the South since the Civil War",
+    "Warren Harding": "Ordered desegregation of the Whitehouse and the District of Columbia",
+    "Calvin Coolidge": "Was known for being a man of very few words",
+    "Herbert Hoover": "Was a self made millionaire from discovering valuable metals all over the globe",
+    "Franklin Roosevelt": "Was a keen stamp collector",
+    "Harry Truman": "He did not find out about the nations atomic program until after FDR died and he was sworn in as president",
+    "Dwight Eisenhower": "Despite serving for thirty five years in the army and becoming a general, he never once saw active combat",
+    "John Kennedy": "Was the first catholic president",
+    "Lyndon Johnson": "His first career was that of a teacher",
+    "Richard Nixon": "His middlename was Milhous",
+    "Gerald Ford": "He served as president and vice president despite never being elected to office",
+    "Jimmy Carter": "He was the first person in his family to graduate highschool",
+    "Ronald Reagan": "Jellybeans were his favorite snack",
+    "George H. W. Bush": "He was the youngest pilot in the navy while he served",
+    "Bill Clinton": "He has won two grammy awards",
+    "George W Bush": "He is the only president with an MBA",
+    "Barrack Obama": "His first job was at baskin robbins scooping icrecream",
+    "Donald Trump": "He is the only president to have been impeached twice",
+    "Joe Biden": "He is the oldest president in us history"
+
+
+
+
+
+
+}
