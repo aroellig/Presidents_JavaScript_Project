78e66c5bba1843707958f0a859588ba583b68021
your name here <roelliga@yahoo.com>
2022-01-31 11:43:42 -0800

    create president

--- src/index.js
+++ src/index.js
@@ -17,18 +17,57 @@ const pres = ["George Washington", "John Adams", "Thomas Jefferson", "James Madi
 
     function createPresident(img, name, text) {
 
-        let fullImg = `./presidents/president-${img}.jpg`
+        let fullImg = `./presidents/${img}.jpg`
         let president = new President(fullImg, name, text)
 
         presidents.push(president)
     }
 
-    
-    createCustomer(0, 'John', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis neque reprehenderit laborum, corporis explicabo assumenda. Porro impedit consectetur animi, reprehenderit recusandae sapiente at aliquam reiciendis modi ipsam rerum suscipit distinctio?')
-    createCustomer(1, 'Sandy', 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock')
-    createCustomer(2, 'Amy', 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don\'t look even slightly believable.')
-    createCustomer(3, 'Tyrell', 'If you are going to use a passage of Lorem Ipsum, you need to be sure there isn\'t anything embarrassing hidden in the middle of text.')
-    createCustomer(4, 'Wanda', 'Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.')
+    createPresident(washington, 'George Washington', 'He is the only US president to have personally led US soldiers into battle while he was serving as the President of the United States')
+    createPresident(adams, 'John Adams', 'He created the United States Marine Band in 1798 which is the oldest professional musical association in the US')
+    createPresident(jefferson, 'Thomas Jefferson', 'He loved to listen to Mocking Birds sing and he kept them as pets')
+    createPresident(madison, 'James Madison', 'While studying at Princeton he stayed for an extra year which makes him the very first postgraduate student in the US')
+    createPresident(monroe, 'James Monroe', 'Monroe was the US president who purchased Florida from the Spanish for five million')
+    createPresident(johnquincyadams, 'John Quincy Adams', 'Every morning he would swim in the Potamac river for exercise')
+    createPresident(jackson, "Andrew Jackson", "He is the only president to have personally thwarted an assination attempt on his life")
+    createPresident(vanburen, "Martin Van Buren", "He was the first US president to have been born in the United States when it was its own country and not a British colony")
+    createPresident(henryharrison, "William Henry Harrison", "He only served as president for one month, after which he died of pneumonia")
+    createPresident(tyler, "John Tyler", "He had fifteen children the most of any president")
+    createPresident(polk, "James Polk", "For two days every week James Polk held office hours in which any citizen could visit him")
+    createPresident(taylor, "Zachary Taylor", "He was unaware that he was nominated as presential candidate until he recieved a letter telling him so")
+    createPresident(fillmore, "Millard Fillmore", "He did not have a vice president while he was president")
+    createPresident(pierce, "Franklin Pierce", "When he was twelve his father made him walk for miles in a thunderstorm")
+    createPresident(buchanan, "James Buchanan", "He is the only president who has never been married")
+    createPresident(lincoln, "Abraham Lincoln", "Was a very accomplished wrestler and only ever lost one out of three hundred matches")
+    createPresident(johnson, "Andrew Johnson", "Was the first president to ever be impeached")
+    createPresident(grant, "Ulysses Grant", "He was a gifted writer and his autobiography is considered to be one of the best ever written by a former president")
+    createPresident(hayes, "Rutherford Hayes", "He and his wife started the tradition of having Easter Egg rolls on the Whitehouse lawn")
+    createPresident(garfield, "James Garfield", "Married one of his students while he was teaching classes at a college")
+    createPresident(arthur, "Chester Arthur", "He was a very skilled fisherman")
+    createPresident(cleveland, "Grover Cleveland", "Is the only US president to have served two non consectutive terms")
+    createPresident(harrison, "Benjamin Harrison", "Was afraid to touch lightbulbs out of fear of electrocution")
+    createPresident(mckinley, "William McKinley", "He was the first president to ever ride in an automobile")
+    createPresident(teddyroosevelt, "Theodore Roosevelt", "He was blinded in one eye while boxing in the Whitehouse")
+    createPresident(taft, "William Taft", "Was the fattest president and once got stuck in the Whitehouse bathtub")
+    createPresident(wilson, "Woodrow Wilson", "Was the first president from the South since the Civil War")
+    createPresident(harding, "Warren Harding", "Ordered desegregation of the Whitehouse and the District of Columbia")
+    createPresident(coolidge, "Calvin Coolidge", "Was known for being a man of very few words")
+    createPresident(hoover, "Herbert Hoover", "Was a self made millionaire from discovering valuable metals all over the globe")
+    createPresident(fdr, "Franklin Roosevelt", "Was a keen stamp collector")
+    createPresident(truman, "Harry Truman", "He did not find out about the nations atomic program until after FDR died and he was sworn in as president")
+    createPresident(Eisenhower, "Dwight Eisenhower", "Despite serving for thirty five years in the army and becoming a general, he never once saw active combat")
+    createPresident(kennedy, "John Kennedy", "Was the first catholic president")
+    createPresident(johnson, "Lyndon Johnson", "His first career was that of a teacher")
+    createPresident(nixon, "Richard Nixon", "His middlename was Milhous")
+    createPresident(ford, "Gerald Ford", "He served as president and vice president despite never being elected to office")
+    createPresident(carter, "Jimmy Carter", "He was the first person in his family to graduate highschool")
+    createPresident(reagan, "Ronald Reagan", "Jellybeans were his favorite snack")
+    createPresident(hwbush, "George H. W. Bush", "He was the youngest pilot in the navy while he served")
+    createPresident(clinton, "Bill Clinton", "He has won two grammy awards")
+    createPresident(Wbush, "George W Bush", "He is the only president with an MBA")
+    createPresident(obama, "Barrack Obama", "His first job was at baskin robbins scooping icrecream")
+    createPresident(trump, "Donald Trump", "He is the only president to have been impeached twice")
+    createPresident(biden, "Joe Biden", "He is the oldest president in us history")
     
 
     buttons.forEach(function(button){
@@ -53,5 +92,6 @@ const pres = ["George Washington", "John Adams", "Thomas Jefferson", "James Madi
              }
         })
     })
+ 
     
 })()
