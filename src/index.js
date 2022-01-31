cf8d96a8aee04a26560884ae8655753f3c6dc645
your name here <roelliga@yahoo.com>
2022-01-31 10:30:22 -0800

    create president

--- src/index.js
+++ src/index.js
@@ -1,37 +1,57 @@
 const pres = ["George Washington", "John Adams", "Thomas Jefferson", "James Madison", "James Monroe", "John Quincy Adams", "Andrew Jackson", "Martin Van Buren", "William Henry Harrison", "John Tyler", "James Polk", "Zachary Taylor", "Millard Fillmore", "Franklin Pierce", "James Buchanan", "Abraham Lincoln", "Andrew Johnson", "Ulysses Grant", "Rutherford Hayes", "James Garfield", "Chester Arthur", "Grover Celveland", "Benjamin Harrison", "William McKinley", "Theordore Roosevelt", "William Taft", "Woodrow Wilson", "Warren Harding", "Calvin Coolidge", "Herbert Hoover", "Franklin Roosevelt", "Harry Truman", "Dwight Eisenhower", "John Kennedy", "Lyndon Johnson", "Richard Nixon", "Gerald Ford", "Jimmy Carter", "Ronald Reagan", "George H. W. Bush", "Bill Clinton", "George W. Bush", "Barrack Obama", "Donald Trump", "Joe Biden"]
 
-function filterRow(array) {
+(function(){
+    const presidentImage = document.querySelector('#president-img')
+    const presidentName = document.querySelector('#president-name')
+    const presidentText = document.querySelector('#president-text')
+    const buttons = document.querySelectorAll('.btn')
+    let index = 0
+    const presidents = []
 
-    let filterPres = [];
-
-    for (let i = 0; i < pres.length; i++) {
-        if (array.includes(pres[i])) {
-            filterPres.push(pres[i])
-        }
-    }
-    let filterData = [];
-
-    for (let j = 0; j < filterPres.length; j++) {
-        const result = pres.indexOf(filterPres[j]);
-
-
-        filterData.push(dataresult);
+    function President(img, name, text) {
+        this.img = img
+        this.name = name
+        this.text = text
     }
 
-    myChart.data.datasets[0].data = filterData;
-    myChart.data.labels = filterPres;
-  
-    myChart.update();
-}
 
-var washingtonBtn = document.querySelector('.Washington-button');
-var WashingtonInfo = document.querySelector('.washington-info');
-var WashingtonClose = document.querySelector('.washington-close');
+    function createPresident(img, name, text) {
 
-WashingtonBtn.addEventListener('click', function(){
-    WashingtonInfo.classList.add('washington-info');
-});
+        let fullImg = `./presidents/president-${img}.jpg`
+        let president = new President(fullImg, name, text)
 
-washingtonClose.addEventListener('click', function(){
-    WashingtonInfo.classList.remove('washington-active');
-})
+        presidents.push(president)
+    }
+
+    
+    createCustomer(0, 'John', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis neque reprehenderit laborum, corporis explicabo assumenda. Porro impedit consectetur animi, reprehenderit recusandae sapiente at aliquam reiciendis modi ipsam rerum suscipit distinctio?')
+    createCustomer(1, 'Sandy', 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock')
+    createCustomer(2, 'Amy', 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don\'t look even slightly believable.')
+    createCustomer(3, 'Tyrell', 'If you are going to use a passage of Lorem Ipsum, you need to be sure there isn\'t anything embarrassing hidden in the middle of text.')
+    createCustomer(4, 'Wanda', 'Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.')
+    
+
+    buttons.forEach(function(button){
+        button.addEventListener('click', function(e){
+            if (e.target.parentElement.classList.contains('prevBtn')){
+               if(index === 0){
+                    index = customers.length
+               }
+               index--
+               customerImage.src = customers[index].img
+               customerName.textContent = customers[index].name
+               customerText.textContent = customers[index].text
+            }
+            if (e.target.parentElement.classList.contains('nextBtn')){
+                index++
+                if(index === customers.length){
+                     index = 0
+                }
+                customerImage.src = customers[index].img
+                customerName.textContent = customers[index].name
+                customerText.textContent = customers[index].text
+             }
+        })
+    })
+    
+})()
