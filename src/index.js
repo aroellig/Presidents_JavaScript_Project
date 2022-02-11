67aea7aee64e43d1ff38b05c4af1a61f554208c1
your name here <roelliga@yahoo.com>
2022-02-10 16:50:46 -0800

    create president renders correct president

--- src/index.js
+++ src/index.js
@@ -6,8 +6,10 @@ const correctFact = document.querySelector('.correct-fact')
 const overall = document.querySelector(".overall")
 const quiz = document.querySelector(".quiz-box")
 const result = document.querySelector(".result")
+const intro = document.querySelector(".intro")
 const scores = document.querySelector(".score")
-
+const presidentsList = document.querySelector(".presidents-list")
+const presName= document.querySelectorAll(".pres-name")
 let numPresidents = 0;
 let currentPresident;
 let availablePresidents = [];
@@ -24,41 +26,48 @@ function setAvailablePresidents(){
     
 }
 
-function createPresident(president){
-    // questionNumber.innerHTML = numPresidents + 0;
-    // const presidentsIndex = availablePresidents[Math.floor(Math.random() * availablePresidents.length)]
-    // const presidentsIndex = availablePresidents[indexOf(target.getAttribute('id')]
-    currentPresident = president;
-    presidentName.innerHTML = currentPresident.name;
-    const index1 = availablePresidents.indexOf(presidentsIndex);
-    availablePresidents.splice(index1, 1);
-    image.innerHTML = ''
-    if (currentPresident.hasOwnProperty("img")){
-        const img = document.createElement("img")
-        img.src = currentPresident.img
-        image.appendChild(img)
+function createPresident(num){
+    debugger
+    const id = num
+    const presNames = Array.from(presName)
+    currentPresident = presNames[id]
+    currentPresident = currentPresident.innerHTML
+    console.log(currentPresident)
+    let presLen = presidents.length
+    for(let i = 0; i < presLen; i++){
+        debugger
+        if(currentPresident === presidents[i].name){
+            let prez = presidents[i]
+            presidentName.innerHTML = prez.name;
+            image.innerHTML = ''
+            if (prez.hasOwnProperty("img")){
+                const img = document.createElement("img")
+                img.src = prez.img
+                image.appendChild(img)
+            }
+        
+            const factsLength = prez.facts.length;
+            for(let i = 0; i < factsLength; i++){
+                availableFacts.push[i]
+            }
+        
+            presidentFacts.innerHTML = ''
+           for(let j = 0; j< factsLength; j++){
+        
+               const factIndex = availableFacts[Math.floor(Math.random() * availableFacts.length)];
+        
+               const index2 = availableFacts.indexOf(factIndex);
+               availableFacts.splice(index2, 1)
+               const fact = document.createElement('div');
+               fact.innerHTML = prez.facts[i];
+               fact.id = j;
+               fact.className = "fact";
+               presidentFacts.appendChild(fact);
+               fact.setAttribute('onclick', 'getResult(this)')
+           }
+            numPresidents ++ 
+        } 
     }
-
-    const factsLength = currentPresident.facts.length;
-    for(let i = 0; i < factsLength; i++){
-        availableFacts.push[i]
-    }
-
-    presidentFacts.innerHTML = ''
-   for(let i = 0; i< factsLength; i++){
-
-       const factIndex = availableFacts[Math.floor(Math.random() * availableFacts.length)];
-
-       const index2 = availableFacts.indexOf(factIndex);
-       availableFacts.splice(index2, 1)
-       const fact = document.createElement('div');
-       fact.innerHTML = currentPresident.facts[i];
-       fact.id = i;
-       fact.className = "fact";
-       presidentFacts.appendChild(fact);
-       fact.setAttribute('onclick', 'getResult(this)')
-   }
-    numPresidents ++ 
 }
 
 function getNewPresident(){
@@ -143,8 +152,10 @@ function correctAnswer(){
     for(let i = 0; i < allPresidents; i++){
         const correct = document.createElement("div");
         correctFact.appendChild(correct)
+        
     }
 }
+
 
 function updateFact(mark){
     correctFact.children[numPresidents -1].classList.add(mark)
@@ -163,9 +174,28 @@ function finalScore(){
     result.querySelector(".percent").innerHTML = ((points/presidents.length) * 100).toFixed() + "%"
     result.querySelector(".total-score").innerHTML= points +" / " + presidents.length
 }
- window.onload = function() {
+
+function tryAgain(){
+    result.classList.add('hide')
+    quiz.classList.remove('hide')
+    reset()
+    start()
+}
+
+function reset(){
+ numPresidents = 0;
+ points = 0;
+ numTrys = 0;
+}
+
+
+   function start() {
+    intro.classList.add('hide')
+    quiz.classList.remove('hide')
+    presidentsList.classList.remove('hide')
      setAvailablePresidents();
-     getNewPresident();
+    //  getNewPresident();
     correctAnswer();
-    // score()
+//    addPresident();
+//    createPresident();
  }
